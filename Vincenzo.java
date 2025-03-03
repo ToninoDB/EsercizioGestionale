d6969fab03a0a967d9ca289fce3d44898ea6abcd
Vince1000-ui <vincenzorummo4@gmail.com>
2025-03-03 15:15:48 +0100

    Add files via upload

---
+++ Vincenzo.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Vincenzo {
+    
+}
+class Utente {
+    String nome;
+    String password;
+    int punteggio;
+    boolean bloccato;
+
+    public Utente(String nome, String password) {
+        this.nome = nome;
+        this.password = password;
+        this.punteggio = 0;
+
+    }
+}
+class RegistroUtenti {
+    private static ArrayList<Utente> utenti = new ArrayList<>();
+
+    public static boolean registraUtente(String nome, String password) {
+        for (Utente u : utenti) {
+            if (u.nome.equalsIgnoreCase(nome)) return false; // Nome univoco richiesto
+        }
+        utenti.add(new Utente(nome, password));
+        return true;
+    }
+    public static Utente login(String nome, String password) {
+        for (Utente u : utenti) {
+            if (u.nome.equalsIgnoreCase(nome) && u.password.equals(password)) {
+                return u;
+            }
+        }
+        return null;
+    }
+    public static void stampaUtenti() {
+        for (Utente u : utenti) {
+            System.out.println("Nome: " + u.nome + " | Punteggio: " + u.punteggio + " | Bloccato: " + u.bloccato);
+        }
+    }
+}
+
